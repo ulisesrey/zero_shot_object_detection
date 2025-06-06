b8c9bbeea17058d70773e5fe3fa8598ef8383187
ulisesrey <ulises.rey.torne@gmail.com>
2025-06-06 13:39:30 +0200

    add if __name__

--- src/analysis.py
+++ src/analysis.py
@@ -18,3 +18,9 @@ def count_objects(df, threshold=0.5):
     print(f"These are the number unique objects detected above the threshold of {threshold}.")
     print(value_counts)
     return 
+
+if __name__ == "__main__":
+    # Example usage
+    df_path = "results/results.csv"
+    df = pd.read_csv(df_path)
+    count_objects(df, threshold=0.7)
